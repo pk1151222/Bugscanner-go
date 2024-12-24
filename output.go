602d4e2e2aa615aa29e355ec35f5ff0ce0ecdc39
pk1151222 <73013821+pk1151222@users.noreply.github.com>
2024-12-24 17:16:31 +0530

    Create output.go

---
+++ output.go
@@ -0,0 +1,18 @@
+package scanner
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func SaveResults(results []ScanResult, fileName string) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(results)
+}
